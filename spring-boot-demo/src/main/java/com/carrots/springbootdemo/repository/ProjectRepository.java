b76c701db5d5d06f27485587ba89bd9b0d095c92
Rakesh Thomas <rakeshjosethomas@gmail.com>
2019-09-17 22:53:38 +0530

    Project repository

---
+++ spring-boot-demo/src/main/java/com/carrots/springbootdemo/repository/ProjectRepository.java
@@ -0,0 +1,16 @@
+package com.carrots.springbootdemo.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.carrots.springbootdemo.entity.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long>{
+
+	@Override
+	Iterable<Project> findAllById(Iterable<Long> ids);
+
+	
+	
+}
