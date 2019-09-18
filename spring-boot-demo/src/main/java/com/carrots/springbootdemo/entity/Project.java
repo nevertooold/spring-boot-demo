e9ede1839ac464f78ff622a86dddb3581bc721ef
Rakesh Thomas <rakeshjosethomas@gmail.com>
2019-09-18 19:23:14 +0530

    project validation in progress

--- spring-boot-demo/src/main/java/com/carrots/springbootdemo/entity/Project.java
+++ spring-boot-demo/src/main/java/com/carrots/springbootdemo/entity/Project.java
@@ -2,12 +2,17 @@ package com.carrots.springbootdemo.entity;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class Project {
@@ -15,13 +20,22 @@ public class Project {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotBlank(message = "is required")
 	private String projectName;
+	@NotBlank(message = "is required")
+	@Size(min = 4, max = 5, message = "4-5 characters")
+	@Column(updatable = false, unique = true)
 	private String projectIdentifier;
+	@NotBlank(message = "is required")
 	private String description;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date startDate;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date endDate;
 	
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date createdAt;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date updatedAt;
 	
 	public Project() {}
