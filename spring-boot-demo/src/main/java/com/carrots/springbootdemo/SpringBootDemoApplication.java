82c5f1e71d35039cce9dc09fa6dbd8e401a3d009
Rakesh Jose Thomas <rakeshjosethomas@gmail.com>
2019-09-19 14:22:38 +0530

    add context path to application

--- spring-boot-demo/src/main/java/com/carrots/springbootdemo/SpringBootDemoApplication.java
+++ spring-boot-demo/src/main/java/com/carrots/springbootdemo/SpringBootDemoApplication.java
@@ -6,7 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
+	private static final String PATH_PROPERTY = "server.servlet.context-path";
+	private static final String PATH = "/springbootdemo";
+	
 	public static void main(String[] args) {
+		System.setProperty(PATH_PROPERTY, PATH);
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
 
